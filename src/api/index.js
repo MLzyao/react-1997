63e87b47bbef824376874b2ae1e1a331c4b4f56c
yao <2587705012@qq.com>
2019-06-24 18:19:42 +0800

    完成退出返回login 功能

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,20 @@
 import  ajax from './ajax';
+import jsonp from 'jsonp';
 export const reqLogin =(username,password) => ajax('/login',{username,password } , 'POST');
 export const reqValidateUserInfo = (id) => ajax('/validate/user',{id}, 'POST')
+// 请求天气
+ export  const reqWeather = function (){
+  return new Promise((resolve,reject)=>{
+    jsonp(`http://api.map.baidu.com/telematics/v3/weather?location=深圳&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`, {}, function (err, data) {
+      if(!err){
+        const { dayPictureUrl, weather} = data.result[0].weather_data[0];
+        resolve({
+          weatherImg:dayPictureUrl,
+          weather
+        });
+      }else{
+      alert("请求天气失败~")
+      }
+  })
+ })
+ }
--- src/components/header-main/index.jsx
+++ src/components/header-main/index.jsx
@@ -1,37 +1,61 @@
 import React, { Component } from 'react';
 import MyButton from '../my-button';
-import logo from '../../assets/image/logo.png';
+import { withRouter } from 'react-router-dom';
+
+import { Modal } from 'antd';
+
+import { removeItem } from '../../utils/storage-tools'
+import { reqWeather } from '../../api';
 import './index.less';
-export default class HeaderMain extends Component {
+class HeaderMain extends Component {
+  state = {
+    sysTime : Date.now(),
+    weather :"晴",
+    weatherImg: 'http://api.map.baidu.com/images/weather/day/qing.png'
+}
+
+
+    // 退出按钮
+  logout =() =>{
+   Modal.confirm({
+      title: 'Do you want to delete these items?',
+     okText:"确定",
+      CancelText:'取消',
+      onOk:() => {
+        console.log(this);
+       removeItem();
+        // 退出登录
+        this.props.history.replace('/login');
+      },
+
+    });
+  }
+
   render() {
+     const { sysTime , weather , weatherImg} =this.state;
+
     return <div>
     <div className='header-main-top'>
       <span>欢迎，admin</span>
-      <MyButton>退出</MyButton>
+
+      <MyButton onClick={this.logout}>退出</MyButton>
     </div>
+
     <div className='header-main-bottom'>
+
       <span className='header-main-left'>用户管理</span>
+
       <div className='header-main-right'>
+
         <span>{Date.now()}</span>
-        <img src={logo} alt=''/>
-        <span>晴</span>
+
+        <img src={weatherImg} alt='weatherImg'/>
+
+        <span>{weather}</span>
+
       </div>
     </div>
     </div>;
   }
 }
-// export  default  class HeaderMain extends Conponent{
-//   render(){
-//     return <div>
-//       <div className="header-main-top">
-//         <span> 欢迎，admin</span>
-//         <MyButton> 推出</MyButton>
-//         </div>
-//       <div className="header-main-bottom">
-//         <span>{ Date.now()}</span>
-//         <img src={logo}  alt=''/>
-//         <sapn>晴天</sapn>
-//       </div>
-//     </div>
-//   }
-// }
+export  default withRouter(HeaderMain);
